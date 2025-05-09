e569c4bf31c91dc34dd4ed73e27c1052650590c6
domingos0000 <domingosmam@al.insper.edu.br>
2025-05-09 20:00:28 -0300

    criei a classe ItemBox

---
+++ items.py
@@ -0,0 +1,29 @@
+import pygame
+pygame.init()
+WIDTH = 1600
+HEIGHT = 870
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+# esta classe foi baseado no seguinte tutorial:
+TILE_SIZE = 64
+
+img_vida =  pygame.image.load('assets/img/items/vida-removebg-preview.png').convert_alpha()
+img_vida= pygame.transform.scale(img_vida, (100, 100))
+img_espinho = pygame.image.load('assets/img/items/espinho-removebg-preview.png').convert_alpha()
+img_espinho= pygame.transform.scale(img_espinho, (150,200 ))
+img_moeda = pygame.image.load('assets/img/items/moeda-removebg-preview.png').convert_alpha()
+img_moeda= pygame.transform.scale(img_moeda, (100, 100))
+
+item_boxes = {
+    "vida"    : img_vida,
+    "espinho" : img_espinho,
+    "moeda"   : img_moeda
+}
+
+
+class ItemBox(pygame.sprite.Sprite):
+    def _init_(self, item_type,x,y):
+        pygame.sprite.Sprite._init_(self)
+        self.item_type = item_type
+        self.image = item_boxes[self.item_type]
+        self.rect = self.image.get_rect()
+        self.rect.midtop = (x + TILE_SIZE//2, y + (TILE_SIZE - self.image.get_height()))
