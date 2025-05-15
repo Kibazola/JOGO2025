aa0ff0c79ed96c523f90846dbc110fd19be3b1bc
domingos0000 <domingosmam@al.insper.edu.br>
2025-05-14 21:20:08 -0300

    alterei a classe pessoa para que os blocos passem a ser sprites de forma a checar colisões.

--- pessoa.py
+++ pessoa.py
@@ -1,41 +1,70 @@
 import pygame
 
-# Criando a classe Pessoa:
 class Pessoa(pygame.sprite.Sprite):
-    def __init__(self, img_personagem, WIDTH, HEIGHT):
-        # Construtor da classe mãe (Sprite).
-        pygame.sprite.Sprite.__init__(self)
+    def _init_(self, img_personagem, WIDTH, HEIGHT, blocks):
+        super()._init_()
+        self.WIDTH = WIDTH     
+        self.blocks = blocks
+        self.HEIGHT = HEIGHT 
         self.original_image = img_personagem
         self.image = img_personagem
         self.rect = self.image.get_rect()
-        
-        self.rect = self.rect.inflate(-60, -20)
+        self.rect = self.rect.inflate(-15, -20)
         self.rect.centerx = WIDTH / 2
-        
         self.rect.bottom = HEIGHT - 10
+        
+
         self.speedx = 0
+        self.speedy = 0
+        self.jump_force = -15
+        self.gravity = 1
         self.isJump = False
-        self.jumpCount = 15
-        self.jumpSpeed = 8 
+        self.blocks = blocks
         self.WIDTH = WIDTH
-        self.HEIGHT = HEIGHT - 100
+        self.HEIGHT = HEIGHT
+
+        self.facing_right = True  # NOVO: direção inicial
 
     def update(self):
-        if not self.isJump:
-            self.rect.x += self.speedx
-        else:
-            # movimento horizontal no ar
-            if self.speedx != 0:
-                self.rect.x += self.jumpSpeed if self.speedx > 0 else -self.jumpSpeed
+        # Atualiza direção
+        if self.speedx > 0:
+            self.facing_right = True
+        elif self.speedx < 0:
+            self.facing_right = False
 
-            # movimento vertical em forma de arco
-            if self.jumpCount >= -15:
-                neg = 1 if self.jumpCount > 0 else -1
-                self.rect.y -= (self.jumpCount ** 2) * 0.3 * neg
-                self.jumpCount -= 1
-            else:
-                self.jumpCount = 15
-                self.isJump = False
+        # Aplica direção à imagem
+        if self.facing_right:
+            self.image = pygame.transform.flip(self.original_image, True, False)
+        else:
+            self.image = self.original_image
+
+        # Movimento horizontal
+        self.rect.x += self.speedx
+
+        # Colisão horizontal
+        collisions = pygame.sprite.spritecollide(self, self.blocks, False)
+        for block in collisions:
+            if self.speedx > 0:
+                self.rect.right = block.rect.left
+            elif self.speedx < 0:
+                self.rect.left = block.rect.right
+
+        # Gravidade
+        self.speedy += self.gravity
+        self.rect.y += self.speedy
+
+        # Colisão vertical
+        collisions = pygame.sprite.spritecollide(self, self.blocks, False)
+        if collisions:
+            for block in collisions:
+                if self.speedy > 0:
+                    self.rect.bottom = block.rect.top
+                    self.isJump = False
+                elif self.speedy < 0:
+                    self.rect.top = block.rect.bottom
+                self.speedy = 0
+        else:
+            self.isJump = True
 
         # Limites da tela
         if self.rect.right > self.WIDTH:
@@ -44,9 +73,10 @@ class Pessoa(pygame.sprite.Sprite):
             self.rect.left = 0
         if self.rect.bottom > self.HEIGHT:
             self.rect.bottom = self.HEIGHT
+            self.speedy = 0
+            self.isJump = False
 
-        # Imagem virada para a direita/esquerda
-        if self.speedx < 0:
-            self.image = self.original_image
-        elif self.speedx > 0:
-            self.image = pygame.transform.flip(self.original_image, True, False)
+    def jump(self):
+        if not self.isJump:
+            self.speedy = self.jump_force
+            self.isJump = True
